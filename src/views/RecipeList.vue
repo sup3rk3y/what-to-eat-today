eb3046d957c385b3335c31ef5e30b17f936a9468
sup3rk3y <lalosapost@gmail.com>
2021-05-29 17:18:38 +0200

    Added Axios and fixed some file names

---
+++ src/views/RecipeList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="recipes">
+    <h1>Awesome Recipes</h1>
+    <RecipeCard v-for="recipe in recipes" :key="recipe.id" :recipe="recipe" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import RecipeCard from "@/components/RecipeCard.vue";
+
+export default {
+  name: "RecipeList",
+  components: {
+    RecipeCard,
+  },
+  data() {
+    return {
+      recipes: [],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.recipes {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
