40bbacb5f065e663e6edb291f7db34a029d3f24f
sup3rk3y <lalosapost@gmail.com>
2021-05-29 16:36:20 +0200

    Initial push for recipes app

---
+++ src/components/RecipeCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="recipe-card">
+    <h4>{{ recipe.title }}</h4>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RecipeCard",
+  props: {
+    recipe: Object,
+  },
+};
+</script>
+
+<style scoped>
+.recipe-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+
+h4 {
+  margin-bottom: 0;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="recipes">
+    <h1>Awesome Recipes</h1>
+    <RecipeCard v-for="recipe in recipes" :key="recipe.id" :recipe="recipe" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import RecipeCard from "@/components/RecipeCard.vue";
+
+export default {
+  name: "Home",
+  components: {
+    RecipeCard,
+  },
+  data() {
+    return {
+      recipes: [
+        {
+          id: 1,
+          title: "Greek Gyros",
+          description: "Delicious :)",
+          incredients: ["600gr pork", "vinegar"],
+          execution: "",
+          category: ["Greek", "pork", "meat"],
+          rating: 5,
+        },
+        {
+          id: 2,
+          title: "CheeseBurger",
+          description: "WOW :O",
+          incredients: ["350gr ground beef", "bacon"],
+          execution: "",
+          category: ["Fast food", "burger", "meat"],
+          rating: 3,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.recipes {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
