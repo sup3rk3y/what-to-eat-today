f65b4d7732b10090ca490f3cb35d5edb02ee215c
sup3rk3y <lalosapost@gmail.com>
2021-06-01 20:25:52 +0200

    Some text modifications

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav">
-    <router-link to="/">RecipeList</router-link> |
+    <router-link to="/">Recipes</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About this app</h1>
+    <p>
+      This app was created in order to present some of my favorite Greek recipes
+      or other well-known recices but with a bit of Greek twist. Enjoy :D
+    </p>
   </div>
 </template>
 
