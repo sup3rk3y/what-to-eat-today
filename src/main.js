c2d9ebbb26aba2e3f9ed304c0999d1747e71103c
sup3rk3y <lalosapost@gmail.com>
2021-06-02 23:01:41 +0200

    Added bootstrap and font-awesome and other dependencies

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const app = createApp(App);
 app.use(store).use(router).mount("#app");
