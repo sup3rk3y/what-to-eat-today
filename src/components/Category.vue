a43c1583a388e52bb5d7534e8f3555af481f7271
sup3rk3y <lalosapost@gmail.com>
2021-06-02 23:02:39 +0200

    Created separate components for Rating and Categories

---
+++ src/components/Category.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="categories">
+    <h6>Categories:</h6>
+    <span> {{ categories }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  props: {
+    category: null,
+  },
+  computed: {
+    categories() {
+      return this.category.join(", ");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.categories {
+  display: inline-flex;
+}
+
+.categories span {
+  font-size: small;
+  font-weight: bold;
+}
+
+.categories h6 {
+  float: left;
+  padding-right: 10px;
+}
+</style>
---
+++ src/components/Rating.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="rating">
+    <span>Rated:</span>
+    <span class="fa fa-star checked"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Rating",
+};
+</script>
